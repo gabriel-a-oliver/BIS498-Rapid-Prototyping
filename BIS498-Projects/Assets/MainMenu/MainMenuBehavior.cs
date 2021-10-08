d0f450ae05c42ac6f3ab8549cf816fc0b6e88727
Gabriel O <oliver.gabriel.a@hotmail.com>
2021-10-08 15:51:40 -0700

    Main Menu Works and Prototype Scenes are set up

---
+++ BIS498-Projects/Assets/MainMenu/MainMenuBehavior.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    public void PlayPrototype1()
+    {
+        SceneManager.LoadScene("Prototype1");
+    }
+    
+    public void PlayPrototype2()
+    {
+        SceneManager.LoadScene("Prototype2");
+    }
+
+    public void PlayPrototype3()
+    {
+        SceneManager.LoadScene("Prototype3");
+    }
+    
+    public void QuitApp()
+    {
+        Debug.Log("Quit App");
+        Application.Quit();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
