0659e47bdf3469b02a366e0618ca9ae9ca4225ef
Gabriel O <olivergabe@hotmail.com>
2021-10-11 22:53:52 -0700

    flipped up and down audio

--- BIS498-Projects/Assets/Prototype1/Scripts/EventManagerBehavior.cs
+++ BIS498-Projects/Assets/Prototype1/Scripts/EventManagerBehavior.cs
@@ -7,8 +7,12 @@ public class EventManagerBehavior : MonoBehaviour
 
     public delegate void FlipToDownState();
     public static event FlipToDownState flippingDown;
+    public delegate void FlippedToDownState();
+    public static event FlippedToDownState flippedDown;
     public delegate void FlipToUpState();
     public static event FlipToUpState flippingUp;
+    public delegate void FlippedToUpState();
+    public static event FlippedToUpState flippedUp;
 
     public static void FlippingDownBehaviors()
     {
@@ -23,6 +27,21 @@ public class EventManagerBehavior : MonoBehaviour
         if (flippingUp != null)
         {
             flippingUp();
+        }
+    }
+    
+    public static void FlippedUpBehaviors()
+    {
+        if (flippedUp != null)
+        {
+            flippedUp();
+        }
+    }
+    public static void FlippedDownBehaviors()
+    {
+        if (flippedDown != null)
+        {
+            flippedDown();
         }
     }
 }
--- BIS498-Projects/Assets/Prototype1/Scripts/LeverBehavior.cs
+++ BIS498-Projects/Assets/Prototype1/Scripts/LeverBehavior.cs
@@ -10,10 +10,14 @@ public class LeverBehavior : MonoBehaviour
     public bool currentlyFlipping = false;
     [SerializeField] private float flippingLerpDuration = 1f;
     public GameObject leverNeck;
+    public AudioClip flippedUpSound;
+    public AudioClip flippedDownSound;
     private void OnEnable()
     {
         EventManagerBehavior.flippingDown += FlippingDown;
         EventManagerBehavior.flippingUp += FlippingUp;
+        EventManagerBehavior.flippedDown += FlippedDown;
+        EventManagerBehavior.flippedUp += FlippedUp;
     }
     void Start()
     {
@@ -24,29 +28,27 @@ public class LeverBehavior : MonoBehaviour
     {
         EventManagerBehavior.flippingDown -= FlippingDown;
         EventManagerBehavior.flippingUp -= FlippingUp;
+        EventManagerBehavior.flippedDown -= FlippedDown;
+        EventManagerBehavior.flippedUp -= FlippedUp;
     }
     private void FlippingDown()
     {
         downwardState = true;
         upwardState = false;
         Quaternion newRotation = Quaternion.Euler(-135f, 0f, 0f);
-        StartCoroutine(RotateLeverNeck(newRotation, flippingLerpDuration));
-        //leverNeck.transform.Rotate(Vector3.right, -90f);
-        Debug.Log("Now Down State");
+        StartCoroutine(RotateLeverNeck(newRotation, flippingLerpDuration, true));
     }
     private void FlippingUp()
     {
         downwardState = false;
         upwardState = true;
         Quaternion newRotation = Quaternion.Euler(-45f, 0f, 0f);
-        StartCoroutine(RotateLeverNeck(newRotation, flippingLerpDuration));
-        //leverNeck.transform.Rotate(Vector3.right, 90f);
-        Debug.Log("Now Up State");
+        StartCoroutine(RotateLeverNeck(newRotation, flippingLerpDuration, false));
     }
 
     // Help From: https://answers.unity.com/questions/192438/coroutines-and-lerp-how-to-make-them-friends.html
     // NOTE: lerpDuration CANNOT be less than or equal to 0
-    private IEnumerator RotateLeverNeck(Quaternion newRotation, float lerpDuration)
+    private IEnumerator RotateLeverNeck(Quaternion newRotation, float lerpDuration, bool flippingDown)
     {
         currentlyFlipping = true;
         if (lerpDuration <= 0f)
@@ -58,11 +60,31 @@ public class LeverBehavior : MonoBehaviour
         Quaternion originalRotation = leverNeck.transform.rotation;
         while (currentTime <= lerpDuration)
         {
-            leverNeck.transform.rotation = Quaternion.Slerp(originalRotation, newRotation, Mathf.SmoothStep(0, 1, currentTime / lerpDuration)/*currentTime / lerpDuration*/);
+            leverNeck.transform.rotation = Quaternion.Slerp(originalRotation, newRotation, 
+                    Mathf.SmoothStep(0, 1, currentTime / lerpDuration));
             currentTime += Time.deltaTime;
             yield return null;
         }
         leverNeck.transform.rotation = Quaternion.Slerp(originalRotation, newRotation, 1f);
         currentlyFlipping = false;
+        if (flippingDown)
+        {
+            EventManagerBehavior.FlippedDownBehaviors();
+        }
+        else
+        {
+            EventManagerBehavior.FlippedUpBehaviors();
+        }
+    }
+
+    private void FlippedDown()
+    {
+        Debug.Log("Now Down State");
+        AudioSource.PlayClipAtPoint(flippedDownSound, Vector3.zero);
+    }
+    private void FlippedUp()
+    {
+        Debug.Log("Now Up State");
+        AudioSource.PlayClipAtPoint(flippedUpSound, Vector3.zero);
     }
 }
