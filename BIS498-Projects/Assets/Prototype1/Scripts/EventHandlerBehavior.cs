84e693384f108c7a6f5ea3b69c2549f4ce392add
Gabriel O <olivergabe@hotmail.com>
2021-10-15 13:54:32 -0700

    limited prototype to 60 fps

--- BIS498-Projects/Assets/Prototype1/Scripts/EventHandlerBehavior.cs
+++ BIS498-Projects/Assets/Prototype1/Scripts/EventHandlerBehavior.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EventHandlerBehavior : MonoBehaviour
 {
     private GameObject lever;
     private GameObject leverHandle;
-    
+
+    [SerializeField] private int fpsTarget = 60;
+    [SerializeField] private TMP_Text fpsText;
+    [SerializeField] private float deltaTime;
+
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = fpsTarget;
+    }
+
     void Start()
     {
         lever = GameObject.FindGameObjectWithTag("Lever");
@@ -16,6 +29,8 @@ public class EventHandlerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        showFPS();
+        
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit raycastHit;
@@ -36,5 +51,22 @@ public class EventHandlerBehavior : MonoBehaviour
                 }
             }
         }
+    }
+
+    // help from: https://answers.unity.com/questions/1366716/how-to-liimit-fps.html
+    private void checkFPS()
+    {
+        if (Application.targetFrameRate != fpsTarget)
+        {
+            Application.targetFrameRate = fpsTarget;
+        }
+    }
+    
+    // help from: https://answers.unity.com/questions/1189486/how-to-see-fps-frames-per-second.html
+    private void showFPS()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = "Frames Per Second: " + Mathf.Ceil (fps).ToString();
     }
 }
