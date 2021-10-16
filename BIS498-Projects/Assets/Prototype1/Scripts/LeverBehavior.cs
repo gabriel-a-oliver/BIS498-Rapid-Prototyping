5a81a8631631e3ac4eaeacc162b695fddfaf7a98
Gabriel O <olivergabe@hotmail.com>
2021-10-15 17:55:06 -0700

    light changes color when flipping lever

--- BIS498-Projects/Assets/Prototype1/Scripts/LeverBehavior.cs
+++ BIS498-Projects/Assets/Prototype1/Scripts/LeverBehavior.cs
@@ -14,6 +14,15 @@ public class LeverBehavior : MonoBehaviour
     public AudioClip flippedDownSound;
     public AudioClip flippingUpSound;
     public AudioClip flippingDownSound;
+
+
+    [SerializeField] private GameObject light1;
+    [SerializeField] private GameObject light2;
+    [SerializeField] private GameObject light3;
+    [SerializeField] private GameObject light4;
+    [SerializeField] private GameObject light5;
+    
+    
     private void OnEnable()
     {
         EventManagerBehavior.flippingDown += FlippingDown;
@@ -76,10 +85,57 @@ public class LeverBehavior : MonoBehaviour
         }
         float currentTime = 0f;
         Quaternion originalRotation = leverNeck.transform.rotation;
+        Renderer handleRenderer = leverNeck.transform.GetChild(0).GetComponent<Renderer>();
         while (currentTime <= lerpDuration)
         {
             leverNeck.transform.rotation = Quaternion.Slerp(originalRotation, newRotation, 
                     Mathf.SmoothStep(0, 1, currentTime / lerpDuration));
+            
+
+
+            if (flippingDown)
+            {
+                handleRenderer.material.color = Color.Lerp(Color.blue, Color.red, currentTime / lerpDuration);
+                
+                if ((currentTime / lerpDuration) > 1/5f)
+                {
+                    light1.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 1f);
+                }
+                if ((currentTime / lerpDuration) > 2/5f)
+                {
+                    light2.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 1f);
+                }
+                if ((currentTime / lerpDuration) > 3/5f)
+                {
+                    light3.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 1f);
+                }
+                if ((currentTime / lerpDuration) > 4/5f)
+                {
+                    light4.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 1f);
+                }
+            }
+            else
+            {
+                handleRenderer.material.color = Color.Lerp(Color.red, Color.blue, currentTime / lerpDuration);
+                
+                if ((currentTime / lerpDuration) > 1/5f)
+                {
+                    light5.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 0f);
+                }
+                if ((currentTime / lerpDuration) > 2/5f)
+                {
+                    light4.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 0f);
+                }
+                if ((currentTime / lerpDuration) > 3/5f)
+                {
+                    light3.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 0f);
+                }
+                if ((currentTime / lerpDuration) > 4/5f)
+                {
+                    light2.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 0f);
+                }
+            } 
+            
             currentTime += Time.deltaTime;
             yield return null;
         }
@@ -87,10 +143,14 @@ public class LeverBehavior : MonoBehaviour
         currentlyFlipping = false;
         if (flippingDown)
         {
+            handleRenderer.material.color = Color.Lerp(Color.blue, Color.red, 1f);
+            light5.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 1f);
             EventManagerBehavior.FlippedDownBehaviors();
         }
         else
         {
+            handleRenderer.material.color = Color.Lerp(Color.blue, Color.red, 0f);
+            light1.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 0f);
             EventManagerBehavior.FlippedUpBehaviors();
         }
     }
@@ -99,12 +159,18 @@ public class LeverBehavior : MonoBehaviour
     {
         downwardState = true;
         Debug.Log("Now Down State");
+
+        //light1.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 1f);
+        
         AudioSource.PlayClipAtPoint(flippedDownSound, Vector3.zero);
     }
     private void FlippedUp()
     {
         upwardState = true;
         Debug.Log("Now Up State");
+        
+        //light1.GetComponent<Renderer>().material.color = Color.Lerp(Color.blue, Color.red, 0f);
+        
         AudioSource.PlayClipAtPoint(flippedUpSound, Vector3.zero);
     }
 }
