3d593530b9dfa858875c4c803c596f903b54bd4e
Gabriel O <oliver.gabriel.a@hotmail.com>
2022-02-06 18:55:08 -0800

    Revert this push, only temporary to display inputs

--- BIS498-Projects/Assets/Prototype2/Scripts/Actions/AvailableActions/RedFireball.cs
+++ BIS498-Projects/Assets/Prototype2/Scripts/Actions/AvailableActions/RedFireball.cs
@@ -23,6 +23,38 @@ public class RedFireball : Fireball
     public override void PerformActionBehavior()
     {
         Debug.Log("Performing ability: " + actionName);
+        
+        GameObject uiInputStream = GameObject.Find("InputStream");
+        
+        GameObject firstBox = (GameObject)Resources.Load("4Direction", typeof(GameObject));
+        //GameObject.Destroy(uiInputStream.transform.GetChild(0).transform.GetChild(0));
+        Object.Instantiate(firstBox, uiInputStream.transform.GetChild(0).transform);
+        //GameObject.Destroy(firstBox, 2f);
+        
+        GameObject secondBox = (GameObject)Resources.Load("1Direction", typeof(GameObject));
+        //GameObject.Destroy(uiInputStream.transform.GetChild(1).transform.GetChild(0));
+        Object.Instantiate(secondBox, uiInputStream.transform.GetChild(1).transform);
+        //GameObject.Destroy(secondBox, 2f);
+        
+        GameObject thirdBox = (GameObject)Resources.Load("2Direction", typeof(GameObject));
+        //GameObject.Destroy(uiInputStream.transform.GetChild(2).transform.GetChild(0));
+        Object.Instantiate(thirdBox, uiInputStream.transform.GetChild(2).transform);
+        //GameObject.Destroy(thirdBox, 2f);
+        
+        GameObject fourthBox = (GameObject)Resources.Load("3Direction", typeof(GameObject));
+        //GameObject.Destroy(uiInputStream.transform.GetChild(3).transform.GetChild(0));
+        Object.Instantiate(fourthBox, uiInputStream.transform.GetChild(3).transform);
+        //GameObject.Destroy(fourthBox, 2f);
+        
+        GameObject fiftheBox = (GameObject)Resources.Load("6Direction", typeof(GameObject));
+        //GameObject.Destroy(uiInputStream.transform.GetChild(4).transform.GetChild(0));
+        Object.Instantiate(fiftheBox, uiInputStream.transform.GetChild(4).transform);
+        
+        GameObject sixthBox = (GameObject)Resources.Load("BButton", typeof(GameObject));
+        //GameObject.Destroy(uiInputStream.transform.GetChild(5).transform.GetChild(0));
+        Object.Instantiate(sixthBox, uiInputStream.transform.GetChild(5).transform);
+        
+        //GameObject.Destroy(uiInputStream.transform.GetChild(6).transform.GetChild(0));
     }
     
     public override int GetActionQueueLifeTime()
--- BIS498-Projects/Assets/Prototype2/Scripts/PlayerInput/MasterPlayerController.cs
+++ BIS498-Projects/Assets/Prototype2/Scripts/PlayerInput/MasterPlayerController.cs
@@ -7,6 +7,13 @@ public class MasterPlayerController : MonoBehaviour
     public InputReader _inputReader;
     public InputManager _inputManager;
     public ActionManager _actionManager;
+
+    public GameObject box1;
+    public GameObject box2;
+    public GameObject box3;
+    public GameObject box4;
+    public GameObject box5;
+    public GameObject box6;
     
     
     [SerializeField]private int endLag = 0;
@@ -49,12 +56,51 @@ public class MasterPlayerController : MonoBehaviour
             _actionManager.ActionQueueLifeTimeDecrement();
             if (endLag == 0)
             {
+                
+                //Transform currentBox = box1.transform.GetChild(0);
+                if (box1.transform.childCount > 0)
+                {
+                    Destroy(box1.transform.GetChild(0));
+                }
+                //currentBox = box2.transform.GetChild(0);
+                if (box2.transform.childCount > 0) 
+                {
+                    Destroy(box2.transform.GetChild(0));
+                }
+                //currentBox = box3.transform.GetChild(0);
+                if (box3.transform.childCount > 0)
+                {
+                    Destroy(box3.transform.GetChild(0));
+                }
+                //currentBox = box4.transform.GetChild(0);
+                if (box4.transform.childCount > 0)
+                {
+                    Destroy(box4.transform.GetChild(0));
+                }
+                //currentBox = box5.transform.GetChild(0);
+                if (box5.transform.childCount > 0)
+                {
+                    Destroy(box5.transform.GetChild(0));
+                }
+                //currentBox = box6.transform.GetChild(0);
+                if (box6.transform.childCount > 0)
+                {
+                    Destroy(box6.transform.GetChild(0));
+                }
+
+                
+                
+                
                 //Debug.Log("Perform next ability");
                 endLag = _actionManager.PerformNextAbility();
                 //Debug.Log("new endlag: " + endLag);
+                
+                
+                
+                
+                
             }
         }
-        
 
         
 
